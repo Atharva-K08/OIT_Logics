8279aa3474480249078cf2e9533efa210a070078
Atharva Kumbhar <111524252+Atharva-K08@users.noreply.github.com>
2025-05-01 12:29:54 +0530

    Add files via upload

---
+++ Demo/Demo.java
@@ -0,0 +1,12 @@
+public class Demo{
+
+    public static void main(String[] args){
+
+        double r = 20;
+        double Area = 3.14 * r * r;
+
+        System.out.println("Area of Circle is: " + Area);
+
+    }
+
+}
